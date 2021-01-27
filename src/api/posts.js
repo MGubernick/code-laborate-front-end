66c5b6d44169a91d3465bd2c1bd3fec4f7a6f018
pvallerie <petevallerie@gmail.com>
2021-01-27 15:49:19 -0500

    Fix errors caused by merge in app.js and api/posts.js

--- src/api/posts.js
+++ src/api/posts.js
@@ -27,18 +27,22 @@ export const postIndexAll = user => {
   })
 }
 
-<<<<<<<
 // Index user
 export const postIndexUser = user => {
   // console.log('This is post at axios', user)
   return axios({
     url: apiUrl + '/posts/user',
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
 
 // Show
 export const showPost = (id, user) => {
   return axios({
     url: apiUrl + '/posts/' + id,
->>>>>>>
     method: 'GET',
     headers: {
       'Authorization': `Bearer ${user.token}`
